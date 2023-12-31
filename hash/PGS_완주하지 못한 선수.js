7c914d1e2872d7528b38518d8542f154f12b4c94
HyunJaeyeon <bomul0109@gmail.com>
2024-01-01 00:30:21 +0900

    🐛fix: 동명이인 골라내기

--- hash/PGS_완주하지 못한 선수.js
+++ hash/PGS_완주하지 못한 선수.js
@@ -1,12 +1,17 @@
-var completion = ["stanko", "ana", "mislav"];
-var participant = ["mislav", "stanko", "mislav", "ana"];
-// var completion = ["josipa", "filipa", "marina", "nikola"];
-// var participant = ["marina", "josipa", "nikola", "vinko", "filipa"];
-
 function solution(participant, completion) {
-  var answer = "";
-  answer = completion.filter((person) => !participant.includes(person));
-  return answer;
+  // 참가자 완주자 알파벳 순으로 정리함 -> 전부 완주 시, 두 배열의 이름.인덱스가 같을 것이다.
+  participant.sort();
+  completion.sort();
+
+  for (let i = 0; i < participant.length; i += 1) {
+    if (participant[i] !== completion[i]) {
+      return participant[i];
+    }
+  }
 }
 
+var participant = ["mislav", "stanko", "mislav", "ana"];
+var completion = ["stanko", "ana", "mislav"];
+// var completion = ["josipa", "filipa", "marina", "nikola"];
+// var participant = ["marina", "josipa", "nikola", "vinko", "filipa"];
 console.log(solution(participant, completion));
